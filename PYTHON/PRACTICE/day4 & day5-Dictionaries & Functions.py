090695c96f9e047f5103ad591416b2a756f4b883
kakarlayathin <kakarlayathin@gmail.com>
2026-09-12 15:38:03 +0530

    Day 4-5: Dictionaries and Functions (AI/ML Plan)

---
+++ PYTHON/PRACTICE/day4 & day5-Dictionaries & Functions.py
@@ -0,0 +1,12 @@
+#DAY-4 :DICTIONARIES
+phonebook = {
+    "mom" : 7702684918,
+    "dad" : 703035770
+}
+print("mom's number is: ", phonebook["mom"])
+
+#DAY-5 : FUNTIONS
+def add(a, b):
+    result = a + b
+    return result
+print("addition of 5 and 6 is: ", add(5,6))
