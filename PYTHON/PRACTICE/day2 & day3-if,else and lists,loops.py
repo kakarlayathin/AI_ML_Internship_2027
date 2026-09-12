397557e14c2f4253e5bdde3da012a72b31dfe1f7
kakarlayathin <kakarlayathin@gmail.com>
2026-09-12 13:36:10 +0530

    Day 2-3: Conditions and Lists (AI/ML Plan)

---
+++ PYTHON/PRACTICE/day2 & day3-if,else and lists,loops.py
@@ -0,0 +1,16 @@
+weather = "cool"
+if weather == "cool":
+    print("yes ur correct!!")
+else:
+    print("heyy ur wrong it is soo cold outside!")
+
+foods = ['rice', 'pappu', 'potato']
+for food in foods:
+    print("my favorite food items is: ", food)
+
+nums = [1,3,5,7,9,2,4,6,8,10]
+for num in nums:
+    if num % 2 == 0:
+        print("the number is even.")
+    else:
+        print("the number is odd.")
